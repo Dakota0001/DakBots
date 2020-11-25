4767ffcfa40f58b73f34fc4dd5558279f82a1ef7
Dakota0001 <dakotashepherd@comcast.net>
2020-11-25 17:08:04 -0500

    added minimum path length setting to help fix infinite loops of small paths

--- pathfind.lua
+++ pathfind.lua
@@ -5,17 +5,18 @@ local Radius = 25
 local StepSize = 50
 local MaxStepHeight = 50
 local OffsetAng = 10
+local MinimumPath = 5
 
 local BaseNode = Start
 local CurNode = Start
 local PathNodes = 0
-local LastPathNodes = 0
+local LastPathNodes = MinimumPath
 local Offset = 0
 local Dir = ((End-CurNode):GetNormalized():Angle()+Angle(0,Offset,0)):Forward()*Vector(1,1,0)
 local HitGoal = false
 
-debugoverlay.Cross( Start, 100, 10, Color( 100, 255, 100 ), true )
-debugoverlay.Cross( End, 100, 10, Color( 255, 100, 100 ), true )
+debugoverlay.Cross( Start, 100, 10, Color( 0, 255, 0 ), true )
+debugoverlay.Cross( End, 100, 10, Color( 255, 0, 0 ), true )
 local NodeList = {BaseNode}
 local runs = 0
 while HitGoal == false and runs < 10000 do
@@ -41,7 +42,7 @@ while HitGoal == false and runs < 10000 do
 			Offset = 0
 		end
 		CurNode = BaseNode
-		LastPathNodes = PathNodes
+		LastPathNodes = math.max(PathNodes,MinimumPath)
 		PathNodes = 0
 	else
 		--run trace down to see if area can be stood in
@@ -78,18 +79,18 @@ while HitGoal == false and runs < 10000 do
 			else
 				CurNode = BaseNode
 			end
-			LastPathNodes = PathNodes
+			LastPathNodes = math.max(PathNodes,MinimumPath)
 			PathNodes = 0
 		else
 			CurNode = Vector(Check.HitPos.x,Check.HitPos.y,CheckDown.HitPos.z+20)
-			debugoverlay.Cross( CurNode, 10, 10, Color( 255, 100, 100 ), true )
+			debugoverlay.Cross( CurNode, 10, 10, Color( 0, 0, 0 ), true )
 			PathNodes = PathNodes + 1
 			if PathNodes > LastPathNodes and Offset > 0 then
 				Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
 				BaseNode = CurNode
 				debugoverlay.Line( NodeList[#NodeList], BaseNode, 10, Color( 100, 100, 255 ), true)
 				NodeList[#NodeList+1] = BaseNode
-				debugoverlay.Cross( BaseNode, 100, 10, Color( 100, 100, 255 ), true )
+				debugoverlay.Cross( BaseNode, 100, 10, Color( 0, 0, 255 ), true )
 				Offset = 0
 			end
 		end
@@ -98,7 +99,7 @@ while HitGoal == false and runs < 10000 do
 		BaseNode = CurNode
 		debugoverlay.Line( NodeList[#NodeList], BaseNode, 10, Color( 100, 100, 255 ), true)
 		NodeList[#NodeList+1] = BaseNode
-		debugoverlay.Cross( BaseNode, 100, 10, Color( 100, 100, 255 ), true )
+		debugoverlay.Cross( BaseNode, 100, 10, Color( 0, 0, 255 ), true )
 		HitGoal = true
 	end
 end
