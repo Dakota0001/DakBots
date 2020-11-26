e529d5f39aaa7786ad88bdb6b287bbbd6aa49a33
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-11-26 01:49:35 -0300

    Improved node creation and storing
    - Nodes will now be indexed by a string instead of a vector, due to the latter producing multiple entries of the same thing.
    - We'll now round instead of floor the coordinate values.
    - A node will now store its center and the floor position.
    - If a node has no floor position, it'll be stored as an unused node on a different table.
    - Displaying the nodes will now use render functions.

--- lua/autorun/client/pathfind_test.lua
+++ lua/autorun/client/pathfind_test.lua
@@ -1,5 +1,6 @@
 DakPath = DakPath or {}
 DakPath.Nodes = DakPath.Nodes or {}
+DakPath.Unused = DakPath.Unused or {}
 
 local StartColor = Color(0, 255, 0)
 local EndColor   = Color(255, 0, 0)
@@ -9,6 +10,7 @@ local PathColor  = Color(0, 0, 255)
 local GridColor  = Color(255, 255, 0)
 
 local Nodes         = DakPath.Nodes
+local Unused        = DakPath.Unused
 local Duration      = 10
 local Radius        = 25
 local StepSize      = 50
@@ -17,41 +19,78 @@ local OffsetAng     = 10
 local Normal        = Vector(1, 1, 0)
 local MinimumPath   = 30
 local MaxIterations = 50000
-local GridSize      = 250
-local GridHeight    = 50
+local GridSize      = 150
+local GridCube      = Vector(GridSize, GridSize, GridSize) * 0.495 -- Leaving a small gap between each of them
+local HalfHeight    = Vector(0, 0, GridSize * 0.5)
+local VectorFormat  = "[%s, %s, %s]"
+
+local CheckNode = {}
+local DownTrace = { start = true, endpos = true, mins = Vector(), maxs = Vector(), mask = MASK_SOLID_BRUSHONLY }
+local WaterTrace = { start = true, endpos = true, mins = Vector(), maxs = Vector(), mask = MASK_WATER }
+local NodeTrace = { start = true, endpos = true, mins = Vector(-Radius, -Radius, -5), maxs = Vector(Radius, Radius, 20), mask = MASK_SOLID_BRUSHONLY, output = CheckNode }
 
 local function GetCoordinate(Position)
-	return Vector(
-		math.floor(Position.x / GridSize),
-		math.floor(Position.y / GridSize),
-		math.floor(Position.z / GridHeight)
-	)
+	return math.Round(Position.x / GridSize), math.Round(Position.y / GridSize), math.Round(Position.z / GridSize)
 end
 
-local function GetPosition(Coordinate)
-	return Vector(
-		math.floor(Coordinate.x * GridSize),
-		math.floor(Coordinate.y * GridSize),
-		math.floor(Coordinate.z * GridHeight)
-	)
+local function AddNode(Position)
+	local X, Y, Z = GetCoordinate(Position)
+	local Key = VectorFormat:format(X, Y, Z)
+
+	if not Nodes[Key] then
+		local Center = Vector(X, Y, Z) * GridSize
+
+		DownTrace.start = Center + HalfHeight
+		DownTrace.endpos = Center - HalfHeight
+
+		local CheckDown = util.TraceHull(DownTrace)
+
+		if not CheckDown.Hit then
+			Unused[Key] = {
+				Center = Center
+			}
+
+			return
+		end
+
+		Nodes[Key] = {
+			Center = Center,
+			Floor = CheckDown.HitPos,
+		}
+	end
 end
 
-local function AddGrid(Position)
-	local Coord = GetCoordinate(Position)
+local function DrawNodes()
+	local Ang = Angle()
 
-	Nodes[Coord] = true
+	for _, Data in pairs(Nodes) do
+		render.DrawWireframeBox(Data.Center, Ang, -GridCube, GridCube, GridColor, true)
+		render.DrawWireframeSphere(Data.Floor, 10, 10, 10, StartColor, true)
+	end
+
+	for _, Data in pairs(Unused) do
+		render.DrawWireframeBox(Data.Center, Ang, -GridCube, GridCube, EndColor, true)
+	end
 end
 
 concommand.Add("path_grid_display", function()
-	for Grid in pairs(Nodes) do
-		debugoverlay.Cross(GetPosition(Grid), 10, Duration, GridColor, true)
-	end
+	hook.Add("PostDrawOpaqueRenderables", "Path Grid Display", DrawNodes)
+
+	timer.Simple(Duration, function()
+		hook.Remove("PostDrawOpaqueRenderables", "Path Grid Display")
+	end)
 end)
 
 concommand.Add("path_grid_clear", function()
-	for Grid in pairs(Nodes) do
-		Nodes[Grid] = nil
+	for Node in pairs(Nodes) do
+		Nodes[Node] = nil
 	end
+
+	for Node in pairs(Unused) do
+		Unused[Node] = nil
+	end
+
+	hook.Remove("PostDrawOpaqueRenderables", "Path Grid Display")
 end)
 
 concommand.Add("path_start", function(Player)
@@ -81,43 +120,38 @@ concommand.Add("path_run", function()
 	local PathNodes     = 0
 	local LastPathNodes = MinimumPath
 	local Offset        = 0
-	local Dir           = (End - CurNode):GetNormalized() * Normal
+	local Direction     = (End - CurNode):GetNormalized() * Normal
 	local HitGoal       = false
 	local NodeList      = { BaseNode }
 	local NodeCount     = 1
 	local Iterations    = 0
 	local DebugStart    = SysTime()
 
-	AddGrid(CurNode)
+	AddNode(CurNode)
 
 	while HitGoal == false and Iterations < MaxIterations do
 		Iterations = Iterations + 1
 
-		local trace = {
-			start = CurNode - Dir * Radius + Vector(0, 0, MaxHeight),
-			endpos = CurNode + Dir * StepSize + Vector(0, 0, MaxHeight),
-			mins = Vector(-Radius, -Radius, -5),
-			maxs = Vector(Radius, Radius, 20),
-			mask = MASK_SOLID_BRUSHONLY,
-		}
-		local Check = util.TraceHull(trace)
-		--debugoverlay.Cross( Check.HitPos, 10, Duration, Color( 255, 255, 100 ), true )
+		NodeTrace.start = CurNode - Direction * Radius + Vector(0, 0, MaxHeight)
+		NodeTrace.endpos = CurNode + Direction * StepSize + Vector(0, 0, MaxHeight)
 
-		if Check.Hit then
-			Offset = Offset + OffsetAng
-			Dir    = ((End - CurNode):GetNormalized():Angle() + Angle(0, Offset, 0)):Forward() * Normal
+		util.TraceHull(NodeTrace)
+
+		if CheckNode.Hit then
+			Offset    = Offset + OffsetAng
+			Direction = ((End - CurNode):GetNormalized():Angle() + Angle(0, Offset, 0)):Forward() * Normal
 
 			if PathNodes > LastPathNodes then
 				debugoverlay.Line(BaseNode, CurNode, Duration, LineColor, true)
 
-				Dir       = (End - CurNode):GetNormalized() * Normal
+				Direction = (End - CurNode):GetNormalized() * Normal
 				NodeCount = NodeCount + 1
 				BaseNode  = CurNode
 				Offset    = 0
 
 				NodeList[NodeCount] = CurNode
 
-				AddGrid(CurNode)
+				AddNode(CurNode)
 
 				debugoverlay.Cross(BaseNode, 100, Duration, NodeColor, true)
 			end
@@ -126,36 +160,26 @@ concommand.Add("path_run", function()
 			LastPathNodes = math.max(PathNodes,MinimumPath)
 			PathNodes = 0
 
-			AddGrid(CurNode)
+			AddNode(CurNode)
 		else
 			--run trace down to see if area can be stood in
-			local downtrace = {
-				start = Check.HitPos + Vector(0, 0, MaxHeight * 2),
-				endpos = Check.HitPos + Vector(0, 0, -10000),
-				mins = Vector(),
-				maxs = Vector(),
-				mask = MASK_SOLID_BRUSHONLY,
-			}
-			local CheckDown = util.TraceHull(downtrace)
+			DownTrace.start = CheckNode.HitPos + Vector(0, 0, MaxHeight * 2)
+			DownTrace.endpos = CheckNode.HitPos + Vector(0, 0, -10000)
+			local CheckDown = util.TraceHull(DownTrace)
 
-			local watertrace = {
-				start = Check.HitPos + Vector(0,0,MaxHeight * 2),
-				endpos = Check.HitPos + Vector(0,0,-10000),
-				mins = Vector(),
-				maxs = Vector(),
-				mask = MASK_WATER,
-			}
-			local Checkwater = util.TraceHull(watertrace)
+			WaterTrace.start = CheckNode.HitPos + Vector(0, 0, MaxHeight * 2)
+			WaterTrace.endpos = CheckNode.HitPos + Vector(0, 0, -10000)
+			local CheckWater = util.TraceHull(WaterTrace)
 
-			if CheckDown.HitPos:Distance(Check.HitPos) < MaxHeight or CheckDown.HitPos:Distance(Check.HitPos) > MaxHeight * 2 or (Checkwater.Hit and Checkwater.HitPos.z > CheckDown.HitPos.z) then
-				--debugoverlay.Cross( Check.HitPos, 40, Duration, Color( 255, 100, 100 ), true )
-				Offset = Offset + OffsetAng
-				Dir    = ((End-CurNode):GetNormalized():Angle() + Angle(0, Offset, 0)):Forward() * Normal
+			if CheckDown.HitPos:Distance(CheckNode.HitPos) < MaxHeight or CheckDown.HitPos:Distance(CheckNode.HitPos) > MaxHeight * 2 or (CheckWater.Hit and CheckWater.HitPos.z > CheckDown.HitPos.z) then
+				--debugoverlay.Cross( CheckNode.HitPos, 40, Duration, Color( 255, 100, 100 ), true )
+				Offset    = Offset + OffsetAng
+				Direction = ((End-CurNode):GetNormalized():Angle() + Angle(0, Offset, 0)):Forward() * Normal
 
 				if PathNodes > LastPathNodes then
 					debugoverlay.Line(BaseNode, CurNode, Duration, LineColor, true)
 
-					Dir       = (End - CurNode):GetNormalized() * Normal
+					Direction  = (End - CurNode):GetNormalized() * Normal
 					NodeCount = NodeCount + 1
 					BaseNode  = CurNode
 					Offset    = 0
@@ -165,8 +189,8 @@ concommand.Add("path_run", function()
 					debugoverlay.Cross(CurNode, 100, Duration, NodeColor, true)
 				end
 
-				if CheckDown.HitPos:Distance(Check.HitPos) > MaxHeight * 2 then
-					if CheckDown.HitPos:Distance(Check.HitPos) > MaxHeight * 3 then
+				if CheckDown.HitPos:Distance(CheckNode.HitPos) > MaxHeight * 2 then
+					if CheckDown.HitPos:Distance(CheckNode.HitPos) > MaxHeight * 3 then
 						CurNode = BaseNode
 					else
 						CurNode = Vector(BaseNode.x,BaseNode.y,CheckDown.HitPos.z + 20)
@@ -178,25 +202,27 @@ concommand.Add("path_run", function()
 				LastPathNodes = math.max(PathNodes,MinimumPath)
 				PathNodes = 0
 
-				AddGrid(CurNode)
+				AddNode(CurNode)
 			else
-				CurNode   = Vector(Check.HitPos.x,Check.HitPos.y,CheckDown.HitPos.z + 20)
+				CurNode   = Vector(CheckNode.HitPos.x,CheckNode.HitPos.y,CheckDown.HitPos.z + 20)
 				PathNodes = PathNodes + 1
 
-				AddGrid(CurNode)
+				AddNode(CurNode)
 
 				debugoverlay.Cross(CurNode, 10, Duration, PathColor, true)
 
 				if PathNodes > LastPathNodes and Offset > 0 then
 					debugoverlay.Line(BaseNode, CurNode, Duration, LineColor, true)
 
-					Dir       = (End - CurNode):GetNormalized() * Normal
+					Direction = (End - CurNode):GetNormalized() * Normal
 					NodeCount = NodeCount + 1
 					BaseNode  = CurNode
 					PathNodes = 0
 					Offset    = 0
 
 					NodeList[NodeCount] = CurNode
+
+					AddNode(CurNode)
 
 					debugoverlay.Cross(CurNode, 100, Duration, NodeColor, true )
 				end
