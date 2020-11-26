5745dbd16ceaa70317fa1cf1319a60d9e700f30c
Dakota0001 <dakotashepherd@comcast.net>
2020-11-25 19:53:30 -0500

    added in path optimization attempt

--- pathfind.lua
+++ pathfind.lua
@@ -51,19 +51,19 @@ while HitGoal == false and runs < 10000 do
 			downtrace.start = Check.HitPos + Vector(0,0,MaxStepHeight*2)
 			downtrace.endpos = Check.HitPos + Vector(0,0,-10000)
 			downtrace.filter = {}
-			downtrace.mins = Vector(-1,-1,-1)
-			downtrace.maxs = Vector(1,1,1)
+			downtrace.mins = Vector(0)
+			downtrace.maxs = Vector(0)
 			downtrace.mask = MASK_SOLID_BRUSHONLY
 		local CheckDown = util.TraceHull( downtrace )
 		local watertrace = {}
 			watertrace.start = Check.HitPos + Vector(0,0,MaxStepHeight*2)
 			watertrace.endpos = Check.HitPos + Vector(0,0,-10000)
 			watertrace.filter = {}
-			watertrace.mins = Vector(-1,-1,-1)
-			watertrace.maxs = Vector(1,1,1)
+			watertrace.mins = Vector(0)
+			watertrace.maxs = Vector(0)
 			watertrace.mask = MASK_WATER
 		local Checkwater = util.TraceHull( watertrace )
-		if CheckDown.HitPos:Distance(Check.HitPos)<=MaxStepHeight or CheckDown.HitPos:Distance(Check.HitPos)>MaxStepHeight*2 or (Checkwater.Hit and Checkwater.HitPos.z>CheckDown.HitPos.z) then
+		if CheckDown.HitPos:Distance(Check.HitPos)<MaxStepHeight or CheckDown.HitPos:Distance(Check.HitPos)>MaxStepHeight*2 or (Checkwater.Hit and Checkwater.HitPos.z>CheckDown.HitPos.z) then
 			--debugoverlay.Cross( Check.HitPos, 40, 10, Color( 255, 100, 100 ), true )
 			Offset = Offset + OffsetAng
 			Dir = ((End-CurNode):GetNormalized():Angle()+Angle(0,Offset,0)):Forward()*Vector(1,1,0)
@@ -88,7 +88,7 @@ while HitGoal == false and runs < 10000 do
 			PathNodes = 0
 		else
 			CurNode = Vector(Check.HitPos.x,Check.HitPos.y,CheckDown.HitPos.z+20)
-			debugoverlay.Cross( CurNode, 10, 10, Color( 0, 0, 0 ), true )
+			--debugoverlay.Cross( CurNode, 10, 10, Color( 0, 0, 0 ), true )
 			PathNodes = PathNodes + 1
 			if PathNodes > LastPathNodes and Offset > 0 then
 				Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
@@ -105,10 +105,103 @@ while HitGoal == false and runs < 10000 do
 		BaseNode = CurNode
 		debugoverlay.Line( NodeList[#NodeList], BaseNode, 10, Color( 255, 255, 255 ), true)
 		NodeList[#NodeList+1] = BaseNode
-		debugoverlay.Cross( BaseNode, 100, 10, Color( 0, 0, 255 ), true )
+		--debugoverlay.Cross( BaseNode, 100, 10, Color( 0, 0, 255 ), true )
 		HitGoal = true
 	end
 end
+
+--run LOS check and ground check steps to each position ahead of current node starting from furthest and stopping at next in line to see if it can directly move there
+local FinalWaypoints = {Start}
+
+CurNode = FinalWaypoints[#FinalWaypoints]
+BaseNode = FinalWaypoints[#FinalWaypoints]
+End = NodeList[#NodeList]
+Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
+local failedpaths = 0
+local lastwaypoint = 1
+HitGoal = false
+local runs2 = 0
+local runs3 = 0
+HitFinalGoal = false 
+while HitFinalGoal == false and runs3 < 10000 do
+	runs3 = runs3 + 1
+
+	CurNode = FinalWaypoints[#FinalWaypoints]
+	BaseNode = FinalWaypoints[#FinalWaypoints]
+	End = NodeList[#NodeList]
+	Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
+	failedpaths = 0
+	runs2 = 0
+	HitGoal = false
+	while HitGoal == false and runs2 < 10000 do
+		runs2 = runs2 + 1
+		local trace = {}
+			trace.start = CurNode - Dir * Radius + Vector(0,0,MaxStepHeight)
+			trace.endpos = CurNode + Dir * StepSize + Vector(0,0,MaxStepHeight)
+			trace.filter = {}
+			trace.mins = Vector(-Radius,-Radius,-5)
+			trace.maxs = Vector(Radius,Radius,30)
+			trace.mask = MASK_SOLID_BRUSHONLY
+		local Check = util.TraceHull( trace )
+		--debugoverlay.Cross( Check.HitPos, 10, 10, Color( 255, 100, 100 ), true )
+		if Check.Hit then
+			CurNode = BaseNode
+			failedpaths = failedpaths + 1
+			End = NodeList[#NodeList-failedpaths]
+			Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
+		else
+			--run trace down to see if area can be stood in
+			local downtrace = {}
+				downtrace.start = Check.HitPos + Vector(0,0,MaxStepHeight*2)
+				downtrace.endpos = Check.HitPos + Vector(0,0,-10000)
+				downtrace.filter = {}
+				downtrace.mins = Vector(0)
+				downtrace.maxs = Vector(0)
+				downtrace.mask = MASK_SOLID_BRUSHONLY
+			local CheckDown = util.TraceHull( downtrace )
+			local watertrace = {}
+				watertrace.start = Check.HitPos + Vector(0,0,MaxStepHeight*2)
+				watertrace.endpos = Check.HitPos + Vector(0,0,-10000)
+				watertrace.filter = {}
+				watertrace.mins = Vector(0)
+				watertrace.maxs = Vector(0)
+				watertrace.mask = MASK_WATER
+			local Checkwater = util.TraceHull( watertrace )
+			if CheckDown.HitPos:Distance(Check.HitPos)<MaxStepHeight or CheckDown.HitPos:Distance(Check.HitPos)>MaxStepHeight*2 or (Checkwater.Hit and Checkwater.HitPos.z>CheckDown.HitPos.z) then
+				if CheckDown.HitPos:Distance(Check.HitPos)>MaxStepHeight*2 then
+					if CheckDown.HitPos:Distance(Check.HitPos)>MaxStepHeight*3 then
+						CurNode = BaseNode
+					else
+						CurNode = Vector(BaseNode.x,BaseNode.y,CheckDown.HitPos.z+20)
+					end
+				else
+					CurNode = BaseNode
+				end
+				failedpaths = failedpaths + 1
+				End = NodeList[#NodeList-failedpaths]
+				Dir = (End-CurNode):GetNormalized()*Vector(1,1,0)
+			else
+				CurNode = Vector(Check.HitPos.x,Check.HitPos.y,CheckDown.HitPos.z+20)
+			end
+		end
+		if (#NodeList-failedpaths) == lastwaypoint+1 then
+			print("thingy")
+		end
+		if CurNode:Distance(End) <= 100 or (#NodeList-failedpaths) == lastwaypoint+1 then
+			debugoverlay.Line( FinalWaypoints[#FinalWaypoints], End, 10, Color( 0, 0, 0 ), true)
+			print(#NodeList-failedpaths)
+			lastwaypoint = (#NodeList-failedpaths)
+			FinalWaypoints[#FinalWaypoints+1] = End
+			HitGoal = true
+		end
+	end
+	if #FinalWaypoints==10 then--FinalWaypoints[#FinalWaypoints]:Distance(NodeList[#NodeList]) <= 100 then
+		HitFinalGoal = true
+	end
+end
+print(NodeList[1])
+debugoverlay.Cross( (NodeList[50]), 100, 10, Color( 0, 0, 255 ), true )
 print("Cycles: "..runs)
 print("Nodes: "..(#NodeList))
+print("Waypoints: "..(#FinalWaypoints))
 --PrintTable(NodeList)
